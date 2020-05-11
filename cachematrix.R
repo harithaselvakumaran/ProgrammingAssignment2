5f3cb8565fdd9afd2087436223c47256608d4827
Haritha Selvakumaran <64729108+harithaselvakumaran@users.noreply.github.com>
2020-05-11 16:33:19 +0530

    Final cachematrix
    The one for review

--- cachematrix.R
+++ cachematrix.R
@@ -1,18 +1,17 @@
-## Put comments here that give an overall description of what your
-## functions do
+## In this example we introduce the <<- operator which can be used to assign a value
+##to an object in an environment that is different from the current environment.
 
 ## This function creates a special object "matrix" that can cache it's on inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-        a
-        i<-NULL
-        set<-function(y){
+        i<-NULL                                          ## Global environment
+        set<-function(y){                                ## sets the value of the matrix
                 x<<-y
                 i<<-NULL
         }
-        get<-function() x
-        setinverse<-function(inverse) i<<-inverse
-        getinverse<-function() i
+        get<-function() x                                ##Gets the value of Matrix
+        setinverse<-function(inverse) i<<-inverse        ## Sets the inverse of matrix
+        getinverse<-function() i                         ##Gets the inverse of Matrix
         list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)
 }
 
@@ -23,13 +22,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         i<-x$getinverse()
-        if(!is.null(i)) {
-                message("getting cached data")
+        if(!is.null(i)) {                                ##checks whether inverse is already calculated
+                message("getting cached data")           ##If so, we get the inverse of matrix and computation is skipped
                 return(i)
         }
-        data<-x$get()
-        i<-solve(data,...)
-        x$setinverse(i)
+        data<-x$get()                                     
+        i<-solve(data,...)                               ##Otherwise, inverse is calculated
+        x$setinverse(i)                                  ##and it's value is set in the cache via setinverse function
         i
         
 }
